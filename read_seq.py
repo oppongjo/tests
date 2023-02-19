9df9823101e8452fcb3b4fcbec30df183b2a9d53
oppongjo <122691597+oppongjo@users.noreply.github.com>
2023-02-18 22:41:11 -0500

    Delete read_seq.py

--- read_seq.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# read_seq.py
-from Bio import SeqIO
-
-infile = "/work/courses/BINF6308/data_BINF6308/Module4/dmel-all-chromosome-r6.17.fasta"
-
-for record in SeqIO.parse(infile, "fasta"):
-    print(record.id) 
