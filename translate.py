bb526ba8fa91a16114d91f4576045655bc4fdf28
oppongjo <122691597+oppongjo@users.noreply.github.com>
2023-02-23 00:20:32 -0500

    Delete translate.py

--- translate.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# translate.py
-# Import Seq
-from Bio.Seq import Seq
-dna = Seq("AGTACACTGGTA")
-rna = dna.transcribe()
-protein = rna.translate()
-print(protein)
-SeqIO.write(protein, "protein.fasta", "fasta")
