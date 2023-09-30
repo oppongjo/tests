114cf12a5f9762cec8327b982e65e7d7d8bb5ec6
oppongjo <122691597+oppongjo@users.noreply.github.com>
2023-09-29 21:37:11 -0400

    Add files via upload

--- BioPython_seqio.py
+++ BioPython_seqio.py
@@ -1,8 +1,37 @@
 import sys
 from Bio import SeqIO
-from Bio.SeqRecord import SeqRecord
-input_filename = sys.argv[1]
-output_filename = sys.argv[2]
-with open(output_filename, 'wa') as handle:
-    for rec in SeqIO.parse(input_filename, 'fasta'):
-        handle.write(SeqRecord(id=rec.id, seq=rec.seq.reverse_complement()).format('fasta'))
+
+def reverse_complement(seq):
+  """Returns the reverse complement of a sequence."""
+  complement_map = {"A": "T", "T": "A", "G": "C", "C": "G"}
+  reversed_complement = ""
+  for base in seq[::-1]:
+    reversed_complement += complement_map[base]
+  return reversed_complement
+
+
+  def main():
+  """Takes two arguments from the command line:
+    * The name of the original multi-sequence FASTA file.
+    * The desired name of the new multi-sequence FASTA file.
+  """
+  
+  input_fasta_file = sys.argv[1]
+  output_fasta_file = sys.argv[2]
+
+  # Read the multi-sequence FASTA file.
+  input_records = SeqIO.parse(input_fasta_file, "fasta")
+
+  # Reverse the complements of the sequences.
+  output_records = []
+  for record in input_records:
+    output_record = record.copy()
+    output_record.seq = reverse_complement(record.seq)
+    output_records.append(output_record)
+
+  # Output the new multi-sequence FASTA file.
+  SeqIO.write(output_records, output_fasta_file, "fasta")
+
+
+if __name__ == "__main__":
+  main()
