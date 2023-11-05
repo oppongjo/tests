fb170f88e386b02e7b26f204f37b17082a5771b3
oppongjo <122691597+oppongjo@users.noreply.github.com>
2023-11-05 14:42:54 -0500

    Delete clustalAlign.sh

--- clustalAlign.sh
+++
@@ -1,7 +0,0 @@
-from Bio import SeqIO
-from Bio.Seq import Seq
-
-clustalo_cline = ClustalOmegaCommandline(infile=apoe_aa.fasta, outfile="aligned_sequences.fasta", verbose=True, auto=True)
-stdout, stderr = clustalo_cline()
-
-print("alignment complete.")
